412e44bca9f808626450ac6079f2d0485a52ce3f
Lamberto <lamberto@RyRII>
2015-10-19 19:10:00 -0600

    (2015.10.19) Se agrega despliegue de menu

--- proyectos/workspace/ProyectoSistemaGestionBoletos/src/gestiondboletos/Bol.java
+++ proyectos/workspace/ProyectoSistemaGestionBoletos/src/gestiondboletos/Bol.java
@@ -7,7 +7,9 @@ import java.util.Vector;
 public class Bol {
 	public String evento;
 	public Vector<Boleto> VBoleto;
-	public Bol(){ }
+	public Bol(){
+		VBoleto=new Vector<Boleto>();
+	}
 	public Bol(String evento){
 		this.evento=evento;
 	}
--- proyectos/workspace/ProyectoSistemaGestionBoletos/src/gestiondboletos/TestBoleto.java
+++ proyectos/workspace/ProyectoSistemaGestionBoletos/src/gestiondboletos/TestBoleto.java
@@ -6,23 +6,50 @@ import java.util.Scanner;
 public class TestBoleto {
 
 	public static void main(String[] args) {
+		MENU=new Vector<String>();
 		MENU.add("Generar Boleto");
 		MENU.add("Salir");
+		EVENTO=new Vector<String>();
 		EVENTO.add("Concierto de Iron Maiden");
 		EVENTO.add("Concierto de Ariana Grande");
+		Bol ARR[]=new Bol[2];
+		ARR[0]=new Bol();
+		ARR[1]=new Bol();
+		procesarComandos(ARR);
 	}
 	
 	static void procesarComandos(Bol B[]){
 		int opc, evt;//opcion, evento
-		Bol b;
+		Boleto b;
+		do{
 		opc=mostrarMenu();
 		switch(opc){
 		  case 0:{
 			  evt=mostrarEventos();
-			  b=new Boleto(EVENTO.get(evt));
+			  switch(evt){
+			    case 0:{
+			      b=new Boleto(EVENTO.get(0));
+			      b.numdbol=B[0].VBoleto.size()+1;
+			      B[0].VBoleto.add(b);
+			      break;
+			    }
+			    case 1:{
+			      b=new Boleto(EVENTO.get(1));
+				  b.numdbol=B[1].VBoleto.size()+1;
+				  B[1].VBoleto.add(b);
+			      break;
+			    }
+			    default: break;
+			  }
 			  break;
-		  } 
+		  }
+		  case 1:{
+			  
+			  break;
+		  }
+		  default: break;
 		}
+		}while(opc!=MENU.size()-1);
 	}
 	static Vector<String> MENU;
 	static Vector<String> EVENTO;
@@ -42,6 +69,7 @@ public class TestBoleto {
 		System.out.println("Seleccione evento");
 		for(int i=0; i<EVENTO.size(); ++i)
 			System.out.println(i+" "+EVENTO.get(i));
+		System.out.print("Numero de evento: ");
 		Scanner T=new Scanner(System.in);
 		ev=T.nextInt();
 		return ev;
