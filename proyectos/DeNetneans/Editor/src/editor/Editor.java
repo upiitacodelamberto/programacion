d7c1a4e4dfd507438c7d473c3d7d52649a8437c5
root <root@localhost.localdomain>
2015-11-10 19:25:49 -0600

    (2015.11.10) Se agrega el proyecto (Netbeans) Editor

---
+++ proyectos/DeNetneans/Editor/src/editor/Editor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+/**
+ *
+ * @author Sala2
+ */
+public class Editor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ proyectos/DeNetneans/Editor/src/editor/edicion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+/**
+ *
+ * @author Quantum
+ */
+public class edicion {
+    
+    private void cursiva() {
+        
+     bloc obj=new bloc();   
+     
+     /*if(pes1.isShowing()==true){
+     Font f = pes1.getFont(); 
+     pes1.setFont(new Font(f.getFontName(),Font.ITALIC,f.getSize()));    
+     }
+     if(pes2.isShowing()==true){
+     Font f = pes2.getFont(); 
+     pes2.setFont(new Font(f.getFontName(),Font.ITALIC,f.getSize()));   
+     } 
+     if(pes3.isShowing()==true){
+     Font f = pes3.getFont(); 
+     pes3.setFont(new Font(f.getFontName(),Font.ITALIC,f.getSize()));       
+     } */ 
+}
+    
+    
+    
+}
